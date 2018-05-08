1ab1813f74e0b8bbfe676412d2b1478872fdf63d
root <vikram@nanocorporation.co.uk>
2018-05-08 16:52:52 +0530

    Adding some styles

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,10 +1,85 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name:{{name}}</div>
-    <div class="dashboard-text">roles:<span v-for='role in roles' :key='role'>{{role}}</span></div>
+      <el-row>
+          <el-col :span="24">
+            <div class="grid-content bg-purple">
+              <el-container>
+                <el-aside>
+                  <iframe width="200" height="200" src="https://www.youtube.com/embed/videoseries?list=PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                </el-aside>
+                <el-main>Viedeo Title</el-main>
+              </el-container>
+            </div>
+          </el-col>
+        <!-- <el-col :span="12"><div class="grid-content bg-purple-light"> I am the Details</div></el-col> -->
+      </el-row>
+
+    <!-- <el-row>
+      <el-col :span="12"><div class="grid-content bg-purple-dark">sdf</div></el-col>
+    </el-row> -->
   </div>
 </template>
-
+<style>
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
 <script>
 import { mapGetters } from 'vuex'
 
